91392fad2a6b9b963185b10f0f34f8d76580ab0c
Leto <leto.van-ruymbeke@ensta.fr>
2026-01-27 10:04:57 +0100

    ajout de la classe Particle définissant nos planètes, dans le fichier header

--- main.hpp
+++ main.hpp
@@ -6,6 +6,13 @@ using namespace std;
 class Particle{
     public:
         double position[3];
+        double speed[3];
+        double force[3];
+        double ** film;
+        int film_length;
+        Particle * next;
+
+
 };
 
 
